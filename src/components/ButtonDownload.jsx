1fffecf89199d9224cab56896390e6917d875679
ugochukwuSylva <sylvaspecial@gmail.com>
2024-03-27 00:37:52 +0100

    added active link feature

--- src/components/ButtonDownload.jsx
+++ src/components/ButtonDownload.jsx
@@ -2,7 +2,7 @@ import "./ButtonDownload.scss";
 
 function ButtonDownload({ children }) {
   return (
-    <a hrefs="./MY_RESUME.pdf" download="UGOCHUKWU_RESUME" className="resume">
+    <a href="./MY_RESUME.pdf" download="UGOCHUKWU_RESUME" className="resume">
       {children}
     </a>
   );
